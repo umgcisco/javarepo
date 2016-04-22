7a6bd9d8cc2db8e9b564db998c0a4cbe37915e92
Marco Rivas <mackriv@gmail.com>
2016-04-22 16:07:13 -0600

    primera modificacion al main

--- src/segundoproyecto/Segundoproyecto.java
+++ src/segundoproyecto/Segundoproyecto.java
@@ -17,6 +17,7 @@ public class Segundoproyecto {
      */
     public static void main(String[] args) {
         // TODO code application logic here
+        System.out.println("Hooola SVn");
     }
     
 }
